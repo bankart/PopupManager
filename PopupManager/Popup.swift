55540357b4e03ecf671f9b2c0750543358cbd776
bankart81 <bankart81@icloud.com>
2016-06-24 02:14:46 +0900

    구조변경중
    PopupView 를 내부에  띄움

--- PopupManager/Popup.swift
+++ PopupManager/Popup.swift
@@ -9,28 +9,51 @@
 import UIKit
 import SnapKit
 
+typealias DelayedAction = () -> ()
+
 // MARK: - class
 class Popup: UIView {
     
     private let defaultSize: CGSize = CGSizeMake(280.0, 200.0)
-    static let sharedInstance: Popup = Popup.init(frame: CGRectMake(0.0, 0.0, 280.0, 220.0))
+    private static let _sharedInstance: Popup = Popup.init(frame: UIScreen.mainScreen().bounds)
+    static func sharedInstance() -> Popup {
+        return _sharedInstance
+    }
 
-    var buttonItems:[PopupItemBase]?
+    var popupItems:[PopupItem]?
     var confirmButtonItem:PopupButtonItem?
     var cancelButtonItem:PopupButtonItem?
-    
+    private var popupView: UIView = UIView(frame: CGRectMake(0.0, 0.0, 280.0, 200.0))
+    private var action: DelayedAction!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        let compareFrame = CGRectMake(frame.origin.x, frame.origin.y, defaultSize.width, defaultSize.height)
-        if !CGRectContainsRect(frame, compareFrame) {
-            self.frame = compareFrame
-            setNeedsLayout()
-        }
+        self.popupView.backgroundColor = UIColor.darkGrayColor()
+        self.popupView.layer.cornerRadius = 7
+        self.popupView.layer.borderWidth = 2
+        self.popupView.layer.borderColor = UIColor.redColor().CGColor
+        
+        self.addSubview(self.popupView)
+        NSLog("Popup: \(self)")
+        NSLog("PopupView: \(self.popupView)")
+//        let compareFrame = CGRectMake(frame.origin.x, frame.origin.y, defaultSize.width, defaultSize.height)
+//        if !CGRectContainsRect(frame, compareFrame) {
+//            self.frame = compareFrame
+//            setNeedsLayout()
+//        }
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func didMoveToSuperview() {
+        if self.superview != nil {
+            self.popupView.snp_makeConstraints(closure: { (make) in
+                make.center.equalTo(self)
+            })
+            NSLog("snapkitting")
+        }
     }
     
     // MARK: use tuple
@@ -71,40 +94,117 @@ class Popup: UIView {
 
 extension Popup {
     
-    func showWithItems(items:[PopupItemBase]) {
+    func hide() {
         
-        self.snp_makeConstraints { (make) in
-            make.center.equalTo(self.superview!)
-        }
-        
-        self.buttonItems = items
-        guard let buttons = self.buttonItems else {
-            NSLog("have no button items")
-            return
-        }
-        
-        let buttonCount: Int = buttons.count
-        let buttonWidth: CGFloat = self.bounds.size.width / CGFloat(buttonCount)
-        buttons.enumerate().forEach { (index, item) in
-            let btn: UIButton = UIButton.init(type: UIButtonType.Custom)
-            var h: CGFloat = buttonWidth
-            if let s: CGSize = item.size {
-                if s.height > buttonWidth {
-                    h = s.height
-                }
-            }
-            btn.frame = CGRectMake(0.0, 0.0, buttonWidth, h)
-            btn.setTitle(item.title, forState: UIControlState.Normal)
-            self.addSubview(btn)
-            btn.snp_makeConstraints(closure: { (make) in
-                make.bottom.equalTo(self)
-                make.left.equalTo(self).inset(buttonWidth*CGFloat(index))
-            })
+        let parent = rootView()
+        if parent.subviews.contains(self.popupView) {
+            self.popupView.removeFromSuperview()
+        } else {
+            NSLog("already hided")
         }
         
     }
     
+    func showWithItems(items:[PopupItem]) {
+        
+        self.popupItems = items
+        guard let popupViewItems = self.popupItems else {
+            NSLog("have no popup items")
+            return
+        }
+        
+        self.popupView.removeConstraints(self.popupView.constraints)
+        self.popupView.subviews.enumerate().forEach { (index, element) in
+            element.removeFromSuperview()
+        }
+        
+        let gap: CGFloat = 10.0
+        let buttonItems = popupViewItems.first!.buttonItems
+        let buttonCount: Int = buttonItems.count
+        let buttonWidth: CGFloat = (self.popupView.bounds.size.width - (gap * (2 + CGFloat(buttonCount) - 1))) / CGFloat(buttonCount)
+        let buttonHeight: CGFloat = 30.0
+        NSLog("btnCount: \(buttonCount)")
+        NSLog("btnW: \(buttonWidth), btnH: \(buttonHeight)")
+        buttonItems.enumerate().forEach { (index, item) in
+            let btn = self.makeDefaultButton(CGRectMake(0.0, 0.0, buttonWidth, buttonHeight), title: item.title)
+            btn.setActionTo({ (button) in
+                if let cancelBlock = item.cancel {
+                    cancelBlock(NSError(domain: "Popup", code: 400, userInfo: nil))
+                }
+                if let confirmBlock = item.confirm {
+                    confirmBlock()
+                }
+                }, forEvents: UIControlEvents.TouchUpInside)
+            self.popupView.addSubview(btn)
+            btn.snp_makeConstraints(closure: { (make) in
+                make.bottom.equalTo(self.popupView)
+                if index == 0 {
+                    make.left.equalTo((buttonWidth + gap)*CGFloat(index)).offset(gap)
+                } else {
+                    make.left.equalTo((buttonWidth + gap)*CGFloat(index))
+                }
+            })
+        }
+        
+        let parent = rootView()
+        if parent.subviews.contains(self.popupView) {
+            NSLog("todo: update ui")
+        } else {
+            parent.addSubview(self.popupView)
+        }
+        
+    }
+    
+    func makeDefaultButton(frame: CGRect, title: String) -> UIButton {
+        let btn: UIButton = UIButton.init(type: UIButtonType.Custom)
+        btn.backgroundColor = UIColor.init(colorLiteralRed: 0.45, green: 0.65, blue: 0.15, alpha: 0.45)
+        btn.layer.cornerRadius = 5
+        btn.layer.borderWidth = 2
+        btn.layer.borderColor = UIColor.blackColor().CGColor
+        btn.setTitle(title, forState: UIControlState.Normal)
+        btn.frame = frame
+        return btn
+    }
+    
+    func rootView() -> UIView {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        return appDelegate.window!
+    }
 }
 
 
+private extension UIButton {
+    
+    private func setOrTriggerClosure(closure:((button:UIButton) -> Void)? = nil) {
+        
+        //struct to keep track of current closure
+        struct ClosureStore {
+            static var closure :((button:UIButton) -> Void)?
+        }
+        
+        //if closure has been passed in, set the struct to use it
+        if closure != nil {
+            ClosureStore.closure = closure
+        } else {
+            //otherwise trigger the closure
+            ClosureStore.closure?(button: self)
+        }
+    }
+    
+    @objc private func triggerActionClosure() {
+        self.setOrTriggerClosure()
+    }
+    
+    func setActionTo(closure:(UIButton) -> Void, forEvents :UIControlEvents) {
+        
+        NSLog("execute closure >> \(self.titleLabel?.text!)")
+        closure(self)
+        
+        self.setOrTriggerClosure(closure)
+        self.addTarget(self, action:
+            #selector(UIButton.triggerActionClosure),
+                       forControlEvents: forEvents)
+    }
+    
+}
 
